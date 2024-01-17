38ec49c59c4af41ce619755f85f41bdc6a6614a1
Chukobyte <michaelsmith8293@gmail.com>
2024-01-16 23:49:03 -0500

    Starring 3 random repos once a day.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A repo that will star all repositories of a user who has starred this one.  [Star Bot](https://github.com/star-repo-bot) checks every 30 minutes and will process starring repos accordingly.  Any new repos that a user create after starring this repo will also be starred.
 
-A random repo will also be starred by starred bot once a day.
+Another thing that star bot does is star 3 random repos once a day.
 
 ## Star History
 
--- star_random_repo.py
+++ star_random_repo.py
@@ -6,6 +6,7 @@ from github_api import GithubAPI
 bot_username = "star-repo-bot"
 bot_github_token = os.getenv("STAR_BOT_GITHUB_TOKEN")
 starred_repos_db_file_path = "starred_repos_db.json"
+number_of_repos_to_star = os.getenv("STAR_BOT_NUM_OF_REPOS_TO_STAR", default=3)
 
 print("1. Load star repos db file")
 cached_starred_repos = []
@@ -17,14 +18,20 @@ try:
 except FileNotFoundError:
     print(f"{starred_repos_db_file_path} doesn't exist, skipping loading...")
 
-print("2. Star random repo")
-random_repo_name, random_repo_url, random_repo_username = GithubAPI.get_random_unstarred_repo(bot_username, bot_github_token)
-if GithubAPI.star_repository(random_repo_username, random_repo_name, bot_github_token):
-    print(f"Successfully starred {random_repo_url}")
-else:
-    raise Exception(f"Failed to star repo {random_repo_url}")
+print("2. Star random repos")
+starred_repos = []
+failed_starred_repos = []
+for i in range(number_of_repos_to_star):
+    random_repo_name, random_repo_url, random_repo_username = GithubAPI.get_random_unstarred_repo(bot_username, bot_github_token)
+    if GithubAPI.star_repository(random_repo_username, random_repo_name, bot_github_token):
+        print(f"Successfully starred {random_repo_url}")
+        starred_repos.append(random_repo_url)
+    else:
+        failed_starred_repos.append(random_repo_url)
+if failed_starred_repos:
+    raise Exception(f"Failed to star repos: {failed_starred_repos}")
 
 print("3. Save new starred repo")
-cached_starred_repos.append(random_repo_url)
+cached_starred_repos.extend(starred_repos)
 with open("starred_repos_db.json", 'w') as json_file:
     json.dump({"repos": cached_starred_repos}, json_file, indent=2)
